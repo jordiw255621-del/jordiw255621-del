871fdede47ed650cc4ebeccb7b4d7124d4ad2792
Jordan <jordiw255621@gmail.com>
2026-07-25 20:57:23 +1000

    Set Sydney clock to custom format

--- scripts/update_readme.py
+++ scripts/update_readme.py
@@ -3,13 +3,13 @@ import datetime, pathlib, re
 from zoneinfo import ZoneInfo
 
 now = datetime.datetime.now(ZoneInfo("Australia/Sydney"))
-stamp = now.strftime("🕒 <b>%A, %d %B %Y &nbsp;·&nbsp; %I:%M %p</b> &nbsp;<sub>Sydney time</sub>")
+stamp = now.strftime("<b>%A, %-d %B %Y &nbsp;·&nbsp; %I:%M %p · Sydney time</sub>")
 
 readme = pathlib.Path("README.md")
 text = readme.read_text(encoding="utf-8")
 text = re.sub(
     r"(<!--START_SECTION:time-->).*?(<!--END_SECTION:time-->)",
-    r"\1\n  " + stamp + r"\n  \2",
+    lambda m: m.group(1) + "\n  " + stamp + "\n  " + m.group(2),
     text,
     flags=re.S,
 )
